633e2545246aabfe55215643b0ee2829a89f56ec
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-08-10 18:17:30 +0100

    2023.08.10: Pseudocode

--- p01_pop_calc.java
+++ p01_pop_calc.java
@@ -1,3 +1,15 @@
+/* - Future Population Calculator - 
+- Program to take a user given number of years and return the population after that number of years
+The program assumes the current population is 312,032,486 and that a year is 365 days
+Prompt user for a number of years
+Intialize current population variable
+Create scanner objects for the user input of num of yrs
+Check if the inputs are greater than zero:
+  If so: calculate future population and return to user
+  If not: Inform user and end program
+Close scanner objects
+*/
+
 import java.util.Scanner;
 
 public class p01_pop_calc {
