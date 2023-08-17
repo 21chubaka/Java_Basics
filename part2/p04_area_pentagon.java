6ea7a008ae53b4f6e265d1f7a7410cb13288d828
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-08-17 16:18:59 +0100

    2023.08.17: Area of pent draft

--- part2/p04_area_pentagon.java
+++ part2/p04_area_pentagon.java
@@ -16,5 +16,35 @@ import java.util.Scanner;
 import java.lang.Math.*;
 
 public class p04_area_pentagon {
-    
+    public static void main(String[] args) {
+        // Inform user of rules of program
+        System.out.println("Please enter the length from the center of a pentagon to a vertex.");
+        System.out.println("The length must numeric and be greater than zero.");
+
+        // Scanner for the length
+        Scanner userR = new Scanner(System.in);
+        System.out.println("Enter Length: ");
+        Double r = userR.nextDouble();
+
+        // Check if the length is greater than zero
+        if (r > 0) {
+            //Calculations for Area
+            Double a = Math.PI / 5;
+            Double s = 2 * r * Math.sin(a);
+
+            Double aTop = 5 * (s * s);
+            Double aBot = 4 * Math.tan(a);
+
+            Double area = aTop / aBot;
+
+            System.out.println("The Area of the Pentagon is " + area);
+        } else {
+            // Error message
+            System.out.println("The length you provided is invalid.");
+            System.out.println("The length must be greater than zero.");
+        }
+
+        //Close scanner
+        userR.close();
+    }
 }
