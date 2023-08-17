0f2503b35866d4693c43e25f0349a47b9f3846e3
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-08-17 16:28:51 +0100

    2023.08.17: Pseudocode

--- part2/p05_runway_calc.java
+++ part2/p05_runway_calc.java
@@ -1,3 +1,18 @@
+/* - Program to Return the Minimum Runway Length when Given Speed, Acceleration, and Condition (Wet or Dry) -
+Inform the user of the rules of the program
+Create scanners for speed, acceleration, and condition
+Check if the condition is not valid
+    If so:
+        Inform the user of the mistake
+    If not:
+        Check if the condition is wet
+            If so:
+                Calculate the runway length with 15% increase
+            If not:
+                Calculate the runway length with dry conditions
+Close the scanners for speed, acceleration, and condition
+*/
+
 package part2;
 
 public class p05_runway_calc {
