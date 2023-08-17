c87f1bfab4f09f955cbbe767be71e22b12f60b81
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-08-17 16:15:33 +0100

    2023.08.17: Pseudocode

--- part2/p04_area_pentagon.java
+++ part2/p04_area_pentagon.java
@@ -1,3 +1,14 @@
+/* - Program to Return the Area of the Pentagon when Given the Length from the Center of a Pentagon to a Vertex -
+Inform the user of the rules of the program
+Create scanner for the length
+Check if the length is greater than zero
+    If so:
+        Calculation the are and return it to the user
+    If not:
+        Inform the user of the error
+Close the scanner
+*/
+
 package part2;
 
 public class p04_area_pentagon {
