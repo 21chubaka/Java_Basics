f6a563e4901cb744a2359ec7d7bb817e91a6f2db
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-08-18 12:03:33 +0100

    2023.08.18: Pseudocode

--- part2/p07_day_num.java
+++ part2/p07_day_num.java
@@ -1,3 +1,20 @@
+/* - Program to Return the days in a month given a Year and a month (Checks for Leap Year) -
+Inform the user of the rules of the program
+Create a scanner for the year from the user
+Create a scanner for the month from the user
+Calculate the divisblities of the year by 4, 100, and 400; and store in variables
+Check if the year is greater or equal to zero and the month is between 1 - 12
+    If so:
+        Check if the year is a leap year (div 4 and not div 100) or (div by 4, 100 , & 400)
+            If so:
+                Check the case for the month and return the number of days in that month (leap year specific)
+            If not:
+                Check the case for the month and return the number of days in that month (non-leap year specific)
+    If not:
+        Inform the user of the error (month and/or year)
+Close the scanners
+*/
+
 package part2;
 
 public class p07_day_num {
