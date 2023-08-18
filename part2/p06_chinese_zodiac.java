dcd28e18670b6ec88c2e9c632f49c708c0944c35
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-08-18 11:57:10 +0100

    2023.08.18: Pseudocode

--- part2/p06_chinese_zodiac.java
+++ part2/p06_chinese_zodiac.java
@@ -1,3 +1,15 @@
+/* - Program to Return the Chinese Zodiac Animal given a Year -
+Inform the user of the rules of the program
+Create a scanner for the year from the user
+Check if the year is greater or equal to zero
+    If so:
+        Calculate the remainder
+        Check which remainder case it satisfies and return that animal to the user
+    If not:
+        Inform the user of the error
+Close the scanner
+*/
+
 package part2;
 
 public class p06_chinese_zodiac {
