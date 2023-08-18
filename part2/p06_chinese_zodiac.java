71cdcec2cc08c3fd7a5c7bc63b02d94996f84e3e
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-08-18 11:58:47 +0100

    2023.08.18: Chinese Zodiac draft

--- part2/p06_chinese_zodiac.java
+++ part2/p06_chinese_zodiac.java
@@ -12,6 +12,69 @@ Close the scanner
 
 package part2;
 
+import java.util.Scanner;
+
 public class p06_chinese_zodiac {
-    
+    public static void main(String[] args) {
+        // Inform user of rules of program
+        System.out.println("Please enter a Year and I will tell you that year's Chinese Zodiac Animal.");
+        System.out.println("The year must be positive, numeric, and an integer.");
+
+        // Scanner for Year
+        Scanner userYear = new Scanner(System.in);
+        System.out.println("Enter Year: ");
+        Integer year = userYear.nextInt();
+
+        // Check if the year is greater or equal to zero
+        if (year >= 0) {
+            // Calculate the remainder
+            Integer zodiacAnimal = year % 12;
+
+            // Check the cases for the remainder and return that animal to the user
+            switch (zodiacAnimal) {
+                case 0:
+                    System.out.println(year + " is the year of the monkey.");
+                    break;
+                case 1:
+                    System.out.println(year + " is the year of the rooster.");
+                    break;
+                case 2:
+                    System.out.println(year + " is the year of the dog.");
+                    break;
+                case 3:
+                    System.out.println(year + " is the year of the pig.");
+                    break;
+                case 4:
+                    System.out.println(year + " is the year of the rat.");
+                    break;
+                case 5:
+                    System.out.println(year + " is the year of the ox.");
+                    break;
+                case 6:
+                    System.out.println(year + " is the year of the tiger.");
+                    break;
+                case 7:
+                    System.out.println(year + " is the year of the rabbit.");
+                    break;
+                case 8:
+                    System.out.println(year + " is the year of the dragon.");
+                    break;
+                case 9:
+                    System.out.println(year + " is the year of the snake.");
+                    break;
+                case 10:
+                    System.out.println(year + " is the year of the horse.");
+                    break;
+                case 11:
+                    System.out.println(year + " is the year of the sheep.");
+                    break;
+            }
+        } else {
+            // Error message
+            System.out.println("The year must be positive, numeric, and an integer.");
+        }
+
+        // Close the scanner
+        userYear.close();
+    }
 }
