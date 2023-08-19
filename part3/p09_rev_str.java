1ea9da5ae0577ff1e08c8929016b06b5096c8272
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-08-19 20:57:19 +0100

    2023.08.19: Pseudocode

--- part3/p09_rev_str.java
+++ part3/p09_rev_str.java
@@ -1,3 +1,14 @@
+/* - Program to Read a string from the user and return the string in Reverse -
+Inform the user of the rules of the program
+Create a scanner for the user given string
+Intialize variable to hold a characters and the reversed string
+Loop through the User String character by character:
+    Grab the character are i and put into character variable
+    Add the character to the reversed string variable
+Return the reversed string variable to the User
+Close the scanner
+*/
+
 package part3;
 
 public class p09_rev_str {
