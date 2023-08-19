bee7b4509c0459c96baec1abb8d51a9abe9b7439
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-08-19 20:46:14 +0100

    2023.08.19: Pseudocode

--- part3/p08_vowel_cons.java
+++ part3/p08_vowel_cons.java
@@ -1,3 +1,19 @@
+/* - Program to Read a string from the user and return Number of Vowels & Consonants -
+Inform the user of the rules of the program
+Create a scanner for the User given string and prompt user
+Convert the user string to lowercase and assign to new variable
+Intialize Vowel and Consonant integer variable to zero
+For Loop for the length of the string to go through each character:
+    Check if the character is a vowel:
+        If so:
+            Increment Vowel counter by one
+    Else if check the character is a letter:
+        If so:
+            Increment Consonant counter by one
+Return the Vowel and Consonant counts to the user
+Close the scanner
+*/
+
 package part3;
 
 public class p08_vowel_cons {
