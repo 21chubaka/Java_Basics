d2840425cf85cb142eb3442bd9a9885f148cfd69
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-08-20 16:45:04 +0100

    2023.08.20: Pseudocode

--- part3/p12_guess_num.java
+++ part3/p12_guess_num.java
@@ -1,3 +1,27 @@
+/* - Program to Generate a Random Number and compare Guesses from User -
+ --- Write a program that randomly generates an integer between 0 and 100, inclusive. The 
+program prompts the user to enter a number. Then the program tells the user whether the 
+number is too low, too high or correct
+-----
+Inform the user of the rules of the program
+Create a scanner for the user's guess
+Create a Random Object (inspired from java T point: https://www.javatpoint.com/how-to-generate-random-number-in-java)
+Use the Random Object to generate a random int number bet 0-100 (inclusive) and assign it to a number variable
+Check if the guess is valid (bet 0-100 inclusive):
+    If so:
+        Check if the guess is not correct:
+            If so:
+                Check if the guess is larger than the number:
+                    If so:
+                        Return 'Too High'
+                    If not:
+                        Return 'Too Low'
+            If not:
+                Return 'Correct'
+    If not:
+        Return Error message
+*/
+
 package part3;
 
 public class p12_guess_num {
