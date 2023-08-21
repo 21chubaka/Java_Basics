bf442945bc37e7d5736694fd04a237a20e7e47d3
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-08-21 19:58:22 +0100

    2023.08.21: Four cons ints draft

--- part4/p14_four_con_ints.java
+++ part4/p14_four_con_ints.java
@@ -47,5 +47,71 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class p14_four_con_ints {
-    
+    public static void main(String[] args) {
+        // Inform the user of the rules of the program
+        System.out.println("If you provide an array of integers, I will tell you if the array has (at least) four consecutive integers of the same value.");
+        System.out.println("The numbers you provide must be integers.");
+
+        // Scanner for User to say how many integers they will add
+        Scanner userNumElem = new Scanner(System.in);
+        System.out.println("Enter how many Elements you are going to enter: ");
+
+        if (userNumElem.hasNextInt()) {
+            Integer numElem = userNumElem.nextInt();
+
+            if (numElem >= 0) {
+                // Intialize empty array of the sixe provided
+                int[] numArray = new int[numElem];
+                
+                // Scanner for User to provide elements of array
+                Scanner arrElem = new Scanner(System.in);
+                System.out.println("Enter your Elements to the Array: ");
+
+                // Loop to append elements to array
+                for (int i = 0; i < numElem; i++) {
+                    numArray[i] = arrElem.nextInt();
+
+                }
+
+                // Check if isSorted return true or false
+                if ( isConsecutiveFour(numArray) == true ) {
+                    System.out.println(Arrays.toString(numArray) + " contains four consecutive integers");
+                } else {
+                    System.out.println(Arrays.toString(numArray) + " does not contain four consecutive integers");
+                }
+                // Close the elements of th array scanner
+                arrElem.close();
+            } else {
+                System.out.println("The array must greater or equal to zero.");
+            }
+        } else {
+            System.out.println("All your inputs must be an integer.");
+        }
+
+        //Close the array size scanner
+        userNumElem.close();
+        
+    }
+    public static boolean isConsecutiveFour(int[] arr) {
+        // Intialize boolean variable
+        boolean result = false;
+        // Check if length of the array is less than 4
+        if ( arr.length < 4 ) {
+            result = false;
+        } else {
+            // Loop through the array check if it is sorted (increasing order)
+            for ( int i = 0; i+3 < arr.length; i++ ) {
+                int firstCheck = arr[i];
+                int secondCheck = arr[i + 1];
+                int thirdCheck = arr[i + 2];
+                int fourthCheck = arr[i + 3];
+
+                if ( firstCheck == secondCheck && firstCheck == thirdCheck && firstCheck == fourthCheck ) {
+                    result = true;
+                } 
+            }
+            
+        }
+        return result;
+    }
 }
