7faee6567ac5278a53477ef874a8079b53da833f
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-08-21 18:16:32 +0100

    2023.08.21: Sorted Arr draft

--- part4/p13_sorted_arr.java
+++ part4/p13_sorted_arr.java
@@ -46,5 +46,68 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class p13_sorted_arr {
-    
+    public static void main(String[] args) {
+        // Inform the user of the rules of the program
+        System.out.println("If you provide an array of integers, I will tell you if the array is sorted or not.");
+        System.out.println("The numbers you provide must be integers.");
+
+        // Scanner for User to say how many integers they will add
+        Scanner userNumElem = new Scanner(System.in);
+        System.out.println("Enter how many Elements you are going to enter: ");
+
+        if (userNumElem.hasNextInt()) {
+            Integer numElem = userNumElem.nextInt();
+
+            if (numElem >= 0) {
+                // Intialize empty array of the size provided
+                int[] numArray = new int[numElem];
+                
+                // Scanner for User to provide elements of array
+                Scanner arrElem = new Scanner(System.in);
+                System.out.println("Enter your Elements to the Array: ");
+
+                // Loop to append elements to array
+                for (int i = 0; i < numElem; i++) {
+                    numArray[i] = arrElem.nextInt();
+
+                }
+
+                // Check if isSorted return true or false
+                if ( isSorted(numArray) == true ) {
+                    System.out.println(Arrays.toString(numArray) + " is sorted");
+                } else {
+                    System.out.println(Arrays.toString(numArray) + " is not sorted");
+                }
+                // Close the elements of the array scanner
+                arrElem.close();
+            } else {
+                System.out.println("The array must greater or equal to zero.");
+            }
+        } else {
+            System.out.println("All your inputs must be an integer.");
+        }
+        
+        //Close the array scanner
+        userNumElem.close();
+        
+    }
+    public static boolean isSorted(int[] arr) {
+        // Intialize boolean variable
+        boolean result = false;
+        // Check if length of the array is 1 or 0
+        if ( arr.length == 1 || arr.length == 0 ) {
+            result = true;
+        } else {
+            // Loop through the array check if it is sorted (increasing order)
+            for ( int i = 0; i+1 < arr.length; i++ ) {
+                if ( arr[i] <= arr[i + 1] ) {
+                    result = true;
+                } else {
+                    result = false;
+                }
+            }
+            
+        }
+        return result;
+    }
 }
